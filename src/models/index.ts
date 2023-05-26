cb13e3e5fae294ed62e1d6210a70bad6125bd679
StefanTodorov32 <bgtrak@gmail.com>
2023-05-26 23:42:44 +0300

    fix bug while fetching the channel

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,8 +1,16 @@
 import { DocumentReference } from "firebase/firestore";
 
-export interface Channels {
+export interface Channel {
     name: string;
     description: string;
     createdBy: DocumentReference;
     photoUrl: "string";
+}
+
+export interface Message {
+    text: string;
+    createdBy: DocumentReference;
+    id: string;
+    timestamp: number;
+    sendBy: DocumentReference;
 }
